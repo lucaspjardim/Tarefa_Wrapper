dc1e637493a052fb6899f4d7f0bae32baa569ce6
Lucas Jardim <lucas.pasqualijardim59@gmail.com>
2024-09-21 16:56:54 -0300

    Commit inicial

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int numeroPrimitivo = 42;
+
+        System.out.println("Valor primitivo: " + numeroPrimitivo);
+
+        Integer numeroWrapper = numeroPrimitivo;
+
+        System.out.println("Valor wrapper: " + numeroWrapper);
+    }
+}
